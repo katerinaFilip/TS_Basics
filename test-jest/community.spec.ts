00f12784502b4da764a7dab3e2516b21f8de3e55
katerinaFilip <katerina.filipenua@gmail.com>
2023-07-10 20:39:07 +0300

    add api tests

--- test-jest/community.spec.ts
+++ test-jest/community.spec.ts
@@ -1,16 +1,55 @@
-import Address from "../address";
-import Community from "../community";
-import User from "../user";
+import axios from "axios";
+import { Validator } from "jsonschema";
+import superagent from "superagent"
 
 describe("Test suite", () => {
-    test("Test", async () => {
-        const community = new Community();
-        await community.initialize();
-        const email = 'Shanna@melissa.tv';
-        const expectedUser = new User('Ervin Howell', 'Antonette', 'Shanna@melissa.tv', '010-692-6593 x09125', 'anastasia.net',
-            new Address('Victor Plains', 'Suite 879', 'Wisokyburgh', '90566-7771'));
+    const schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "id": {
+                "type": "integer"
+            },
+            "email": {
+                "type": "string"
+            },
+            "first_name": {
+                "type": "string"
+            },
+            "last_name": {
+                "type": "string"
+            },
+            "avatar": {
+                "type": "string"
+            }
+        },
+        "required": [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "avatar"
+        ]
+    }
 
-        const recievedUser = community.getUserByEmail(email);
-        expect(JSON.stringify(expectedUser) === JSON.stringify(recievedUser)).toBeTruthy();
+    test("Test", async () => {
+        let response: any;
+        const v = new Validator();
+
+        try {
+            response = await superagent.get('https://reqres.in/api/users').query({ id: 2 });
+        }
+        catch (err: any) {
+            console.log(err.message);
+        }
+        expect(response.status).toBe(200);
+        expect(v.validate(response.body.data, schema).valid).toBeTruthy();
     })
+
+    test.only("Test 1", async () => {
+        const response = await axios.post('https://reqres.in/api/users', { name: 'James', job: 'manager' });
+        expect(response.status).toBe(201);
+        expect(response.data.createdAt).toBe(new Date());
+    })
+
 })
